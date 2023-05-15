d2ff9501fef5e1a51e2f9725e267028143484843
MetsalIlnris <MentsalIlnris@2396520904@qq.com>
2023-05-15 23:04:24 +0800

    发布版本V5

--- src/test/java/com/output/controller/user/NewsControllerTest.java
+++ src/test/java/com/output/controller/user/NewsControllerTest.java
@@ -1,9 +1,18 @@
 package com.output.controller.user;
 
+import com.output.OutputApplication;
+import org.junit.FixMethodOrder;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = OutputApplication.class)
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 class NewsControllerTest {
 
     @Test
